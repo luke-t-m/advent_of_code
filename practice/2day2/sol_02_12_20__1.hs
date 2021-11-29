d5798b3c9663fc324e28630a37bad510da09b5d6
luke-t-m <luketmckay@outlook.com>
2021-11-29 17:05:05 +0000

    did day 2, changed file naming convention

---
+++ practice/2day2/sol_02_12_20__1.hs
@@ -0,0 +1,19 @@
+import System.IO
+import Data.List
+import Data.List.Split
+
+
+countNum e x = length (filter (==e) x)
+
+splitter x = delete "" (splitOneOf "- :" x)
+
+checkerH x = checker (splitter x) 
+
+checker :: [String] -> Bool
+checker (a:b:c:d:_) = n >= read a && n <= read b where n = countNum (head c) d
+
+main :: IO ()
+main = do
+ inputs <- readFile "input.txt"
+ let lS = lines inputs
+ print (length (filter (==True) (map checkerH lS)))

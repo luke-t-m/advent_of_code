16f48840f24e9dc87cb71b04627c7c1b590e5d54
luke-t-m <luketmckay@outlook.com>
2021-12-02 05:49:31 +0000

    fixed broken file

--- 0_2020/2day2_haskell/sol_02_12_20__1.hs
+++ 0_2020/2day2_haskell/sol_02_12_20__1.hs
@@ -17,12 +17,3 @@ main = do
  inputs <- readFile "input.txt"
  let lS = lines inputs
  print (length (filter (==True) (map checkerH lS)))
-
-countNum e x = length (filter (==e) x)
-
-splitter x = delete "" (splitOneOf "- :" x)
-
-checkerH x = checker (splitter x) 
-
-checker :: [String] -> Bool
-checker (a:b:c:d:_) = (d!!((read a)-1) == head c) /= (d!!((read b)-1) == head c)

8edb987a18bf3552a974e42c55e54cf29e3b7638
luke-t-m <luketmckay@outlook.com>
2021-12-03 09:30:54 +0000

    further fixed type signatures, simplified chemgr output

--- 1_2021/3day3_haskell/sol_03_12_21.hs
+++ 1_2021/3day3_haskell/sol_03_12_21.hs
@@ -1,21 +1,21 @@
 import System.IO
 
-mlc :: (Int -> Int -> Bool) -> [Char] -> Char
+mlc :: (Int -> Int -> Bool) -> String -> Char
 mlc f x | length (filter (=='1') x) `f` length (filter (=='0') x) = '1'
         | otherwise = '0'
 
-cbs :: [[Char]] -> (Int -> Int -> Bool) -> [Char]
+cbs :: [String] -> (Int -> Int -> Bool) -> String
 cbs x f | length (head x) == 0 = []
         | otherwise = mlc f (map head x) : cbs (map tail x) f
 
-chemgr :: [[Char]] -> Int -> (Int -> Int -> Bool) -> [[Char]]
-chemgr x n f | length x == 1 = x
+chemgr :: [String] -> Int -> (Int -> Int -> Bool) -> String
+chemgr x n f | length x == 1 = head x
              | otherwise = chemgr (filter (\y -> (y!!n)==((cbs x f)!!n)) x) (n+1) f
 
-binToDec :: [Char] -> Int
+binToDec :: String -> Int
 binToDec x = binToDecW (reverse x) 1
 
-binToDecW :: [Char] -> Int -> Int
+binToDecW :: String -> Int -> Int
 binToDecW [] _ = 0
 binToDecW (x:xs) n = n * read [x] + binToDecW xs 2*n
 
@@ -24,4 +24,4 @@ main = do
  input <- readFile "input.txt"
  let x = lines input
  print (binToDec (cbs x (>=) ) * binToDec (cbs x (<) )) -- part 1
- print (binToDec (head ((chemgr x 0 (>=) ))) * binToDec (head ((chemgr x 0 (<) )))) --part 2
+ print (binToDec (chemgr x 0 (>=) ) * binToDec (chemgr x 0 (<) )) --part 2

020c2414786b5f0e7f42680a23a1befc4b64b25e
luke-t-m <luketmckay@outlook.com>
2021-12-04 05:58:41 +0000

    day 4

---
+++ 1_2021/4day4_haskell/solution.hs
@@ -0,0 +1,24 @@
+import System.IO
+import Data.List.Split
+
+makeVerts x | length (head x) == 1 = [map head x]
+            | otherwise = map head x : makeVerts (map tail x) 
+
+winIn (c:cs) m b | [] `elem` b = m
+                 | otherwise = winIn cs (m+1) (map (filter (/=c)) b)
+
+
+main :: IO ()
+main = do
+ doer minimum
+ doer maximum
+
+
+doer f = do
+ input <- readFile "input.txt"
+ let boards = map (filter (/=[])) (tail (map ( map (filter (/="") . splitOn " ") . splitOn "\n") (splitOn "\n\n" input)))
+ let called = splitOn "," (head (lines input))
+ let bboards = [ y ++ (makeVerts y) | y <- boards ]
+ let winner = (f (zip (map (winIn called 0) bboards) boards))
+ let wcalls = (fst (splitAt (fst winner) called))
+ print ((sum [read un | un <- concat (snd winner), not (un `elem` wcalls) ]) * (read (called!!(fst winner -1))))

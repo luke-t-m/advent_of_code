77e9f2b09c1fbe5c7c56e8ff0e6c7c1018ca4dcc
luke-t-m <luketmckay@outlook.com>
2021-12-04 06:19:00 +0000

    tidied brackets and simplified some messy chains of higher order functions

--- 1_2021/4day4_haskell/solution.hs
+++ 1_2021/4day4_haskell/solution.hs
@@ -10,15 +10,15 @@ winIn (c:cs) m b | [] `elem` b = m
 
 main :: IO ()
 main = do
- doer minimum
- doer maximum
+ doer minimum -- part 1
+ doer maximum -- part 2
 
 
 doer f = do
  input <- readFile "input.txt"
- let boards = map (filter (/=[])) (tail (map ( map (filter (/="") . splitOn " ") . splitOn "\n") (splitOn "\n\n" input)))
+ let boards = tail (map (filter (/=[]) . map (filter (/="") . splitOn " ") . splitOn "\n") (splitOn "\n\n" input))
  let called = splitOn "," (head (lines input))
  let bboards = [ y ++ (makeVerts y) | y <- boards ]
- let winner = (f (zip (map (winIn called 0) bboards) boards))
- let wcalls = (fst (splitAt (fst winner) called))
- print ((sum [read un | un <- concat (snd winner), not (un `elem` wcalls) ]) * (read (called!!(fst winner -1))))
+ let winner = f (zip (map (winIn called 0) bboards) boards)
+ let wcalls = fst (splitAt (fst winner) called)
+ print (sum [read un | un <- concat (snd winner), not (un `elem` wcalls) ] * read (called!!(fst winner -1)))

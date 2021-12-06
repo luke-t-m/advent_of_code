48c169c9208f77e18d478b2a7c6f15b237e47638
luke-t-m <luketmckay@outlook.com>
2021-12-06 23:27:38 +0000

    2020 day 9

---
+++ 0_2020/9day9_haskell/solution.hs
@@ -0,0 +1,28 @@
+import Data.List
+import Data.List.Split
+
+subList [] _ = True
+subList (a:as) b = a `elem` b && subList as (delete a b)
+
+combs n x = nub [sort y | y <- sequence (replicate n x), y `subList` x]
+
+
+xmas _ [] = -1
+xmas p@(a:as) (b:bs) | b `elem` (map sum (combs 2 p)) = xmas (as++[b]) bs
+                     | otherwise = b
+
+contiger w ns (b:bs)
+ | length x /= 0 = head x
+ | otherwise = contiger w ([b] : (map (\y -> b:y) ns)) bs
+  where x = filter (\y -> sum y == w) ns
+
+
+main :: IO ()
+main = do
+ input <- readFile "input.txt"
+ let x = splitAt 25 (map read (lines input))
+ let p1 = xmas (fst x) (snd x)
+ print p1
+ let p2 = contiger p1 [] (snd x)
+ print (minimum p2 + maximum p2)
+ print "done."

a7bf8e30a16e74edc01a0040a767cd94642e5beb
luke-t-m <luketmckay@outlook.com>
2021-12-09 07:29:15 +0000

    tidied

--- 1_2021/9day9_haskell/solution.hs
+++ 1_2021/9day9_haskell/solution.hs
@@ -16,13 +16,13 @@ spread g (y,x) = (y,x):[(y+ym,x+xm) | (ym,xm) <- (makelist g (y,x)),
 spreader g l
  | n == l = n
  | otherwise = spreader g n
-  where n =  nub (concat (map (spread g) l))
+  where n =  nub $ concat (map (spread g) l)
 
 main :: IO ()
 main = do
  input <- readFile "input.txt"
  let g = map (map (\y -> read [y]::Int)) (lines input)
  let poss = [ (y,x) | y <- [0..(length g-1)], x <- [0..(length (head g)-1)]]
- print (sum (map (\(y,x) -> (g!!y!!x)+1) (filter (isLow g) poss))) -- part 1
- print (product (fst (splitAt 3 (reverse $ sort (map length (map (\t -> spreader g [t]) poss)))))) -- part 2
+ print (sum $ map (\(y,x) -> (g!!y!!x)+1) (filter (isLow g) poss)) -- part 1
+ print (product $ fst $ splitAt 3 $ reverse $ sort $ map length (map (\t -> spreader g [t]) poss)) -- part 2
  print "done."

d9c701486a7946e3a09189aa8dae85c99f3f91e6
luke-t-m <luketmckay@outlook.com>
2021-12-17 10:55:36 +0000

    more tidying day 17

--- 1_2021/h_day17_haskell/solution.hs
+++ 1_2021/h_day17_haskell/solution.hs
@@ -5,13 +5,11 @@ tax2 = 193
 tay1 = -86
 tay2 = -136
 
-sumTo n = round (m/2 * (m+1)) where m = fromIntegral n
-
 poss xv yv = takeWhile (\(x,y) -> x <= tax2 && y >= tay2) [pos xv yv n | n <- [1..]]
  where pos xv yv n = ((sumTo xv) - (sumTo (max 0 (xv-n))), (sumTo yv) - (sumTo (yv-n)))
+       sumTo n = round (m/2 * (m+1)) where m = fromIntegral n
 
-
-val = filter (\t -> inTA (last t)) (filter (\t -> length t /= 0) [ poss gx gy | gx <- [0..200], gy <- [-150..400]])
+val = filter (\t -> inTA (last t)) (filter (\t -> length t /= 0) [ poss gx gy | gx <- [17..193], gy <- [-136..136]])
  where inTA (x,y) = x >= tax1 && x <= tax2 && y <= tay1 && y >= tay2
 
 maxY = snd (last (sortlast (concat val)))

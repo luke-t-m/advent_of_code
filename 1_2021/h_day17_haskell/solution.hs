acab2028f773b418467214912766184a670964a8
luke-t-m <luketmckay@outlook.com>
2021-12-17 10:46:21 +0000

    tidied code day 17.

--- 1_2021/h_day17_haskell/solution.hs
+++ 1_2021/h_day17_haskell/solution.hs
@@ -1,6 +1,5 @@
 import Data.List
 
-
 tax1 = 150
 tax2 = 193
 tay1 = -86
@@ -8,21 +7,16 @@ tay2 = -136
 
 sumTo n = round (m/2 * (m+1)) where m = fromIntegral n
 
-pos xv yv n = ((sumTo xv) - (sumTo (max 0 (xv-n))), (sumTo yv) - (sumTo (yv-n)))
-
-inTA (x,y) = x >= tax1 && x <= tax2 && y <= tay1 && y >= tay2
-
 poss xv yv = takeWhile (\(x,y) -> x <= tax2 && y >= tay2) [pos xv yv n | n <- [1..]]
+ where pos xv yv n = ((sumTo xv) - (sumTo (max 0 (xv-n))), (sumTo yv) - (sumTo (yv-n)))
 
-rev (a,b) = (b,a)
-sortlast t = map rev (sort (map rev t))
 
 val = filter (\t -> inTA (last t)) (filter (\t -> length t /= 0) [ poss gx gy | gx <- [0..200], gy <- [-150..400]])
-
-splitMaxY ((x,y):(x',y'):r) | y >= y' = [(x',y')]
-                            | otherwise = (x,y) : splitMaxY ((x',y'):r)  
+ where inTA (x,y) = x >= tax1 && x <= tax2 && y <= tay1 && y >= tay2
 
 maxY = snd (last (sortlast (concat val)))
+ where sortlast t = map rev (sort (map rev t))
+       rev (a,b) = (b,a)
 
 main :: IO ()
 main = do

f4346d019a1ade9cb0157b6f95d1603be04f252d
look <luketmckay@outlook.com>
2022-12-01 05:26:24 +0000

    minor improvements to day 1 2022

--- 7_2022/1_day1_python/1.py
+++ 7_2022/1_day1_python/1.py
@@ -4,7 +4,7 @@ lines = open("input").readlines()
 calories = [0]
 for line in lines:
     if line == "\n": calories.append(0)
-    else: calories[len(calories) - 1] += int(line)
+    else: calories[-1] += int(line)
 
 print(f"Part 1: {max(calories)}")
-print(f"Part 2: {sum(sorted(calories)[::-1][:3])}")
+print(f"Part 2: {sum(sorted(calories, reverse = True)[:3])}")

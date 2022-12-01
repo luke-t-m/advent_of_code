5df9b5aac495535afd54298a70e3dde65ec94fee
look <luketmckay@outlook.com>
2022-12-01 17:21:41 +0000

    improved day 1 in spirit of original solution

--- 7_2022/1_day1_python/1.py
+++ 7_2022/1_day1_python/1.py
@@ -6,5 +6,6 @@ for line in lines:
     if line == "\n": calories.append(0)
     else: calories[-1] += int(line)
 
-print(f"Part 1: {max(calories)}")
-print(f"Part 2: {sum(sorted(calories, reverse = True)[:3])}")
+calories.sort()
+print(f"Part 1: {calories[-1]}")
+print(f"Part 2: {sum(calories[:-4:-1])}")

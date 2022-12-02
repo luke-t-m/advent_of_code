9469d6e7b19075881da3b199de1304dff16778e7
luke-t-m <luketmckay@outlook.com>
2022-12-02 16:58:35 +0000

    removed code from the new 2.py from 2_old.py

--- 7_2022/2day_2_python/2_old.py
+++ 7_2022/2day_2_python/2_old.py
@@ -1,11 +1,4 @@
 lines = open("input").readlines()
-
-def asc(char): return int.from_bytes(char.encode(), "big")
-        
-print(f"Part one: {sum(map(lambda l: ((asc(l[2])-asc(l[0])-23)%3+1)%3*3+asc(l[2])-87, lines))}")
-print(f"Part two: {sum(map(lambda l: ((3*(asc(l[2])-89)+asc(l[0])-64)+3+2*(87-asc(l[2])))%3+1+3*(asc(l[2])-88), lines))}")
-
-lines = open("input").readlines()
 #lines = open("test").readlines()
 
 

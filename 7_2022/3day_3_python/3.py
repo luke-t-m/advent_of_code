ca65955030a97e529647cee1798838215cb53ffc
look <luketmckay@outlook.com>
2022-12-04 05:09:55 +0000

    automated file downloading and environment set up for each day and ai solver using gptchat.

--- 7_2022/3day_3_python/3.py
+++
@@ -1,4 +0,0 @@
-input = open("input").readlines()
-
-print(f"Part one: {sum(map(lambda l: (ord((set(l[:len(l)//2])&set(l[len(l)//2:])).pop())-38)%58, input))}")
-print(f"Part two: {sum(map(lambda l: ((ord(((set(l[0])&set(l[1])&set(l[2]))).difference(chr(10)).pop()))-38)%58, zip(input[::3], input[1::3], input[2::3])))}")
---
+++ 7_2022/4_day4_python/4.py
@@ -0,0 +1,21 @@
+#!/usr/bin/env python3
+
+#input = open("4_input").read()
+#input = open("4_test").read()
+overlap_count = 0
+# Open the input file
+with open('4_input') as f:
+    # Iterate over the lines in the file
+    for line in f:
+        # Parse the line to extract the range of section IDs for each pair of Elves
+        elf1, elf2 = line.strip().split(',')
+        elf1_start, elf1_end = map(int, elf1.split('-'))
+        elf2_start, elf2_end = map(int, elf2.split('-'))
+        # Check if the ranges overlap
+        if elf1_start <= elf2_end and elf2_start <= elf1_end:
+            # If the ranges overlap, increment the counter
+            overlap_count += 1
+
+# Print the number of overlapping assignment pairs
+print(overlap_count)
+

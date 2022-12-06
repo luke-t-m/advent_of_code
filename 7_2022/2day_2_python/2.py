b01d92fddf6df166a26f103648250b0dfbf50d27
luke-t-m <luketmckay@outlook.com>
2022-12-06 04:36:33 +0000

    some old advent of code stuff from t440p

--- 7_2022/2day_2_python/2.py
+++ 7_2022/2day_2_python/2.py
@@ -1,6 +1,10 @@
-lines = open("input").readlines()
+import time
 
+lines = open("input").readlines()
+time_start = time.time()
 def asc(char): return int.from_bytes(char.encode(), "big")
         
 print(f"Part one: {sum(map(lambda l: ((asc(l[2])-asc(l[0])-23)%3+1)%3*3+asc(l[2])-87, lines))}")
 print(f"Part two: {sum(map(lambda l: ((3*(asc(l[2])-89)+asc(l[0])-64)+3+2*(87-asc(l[2])))%3+1+3*(asc(l[2])-88), lines))}")
+
+print(time.time() - time_start)

12b5d16ed397c134836b5a713b642b5f2110be2e
look <luketmckay@outlook.com>
2022-12-14 08:12:11 +0000

    dodgy plt animation

--- 7_2022/e_day14_python/14 (copy 1).py
+++ 7_2022/e_day14_python/14 (copy 1).py
@@ -1,8 +1,12 @@
 #!/usr/bin/env python3
 import math
+import time
+import matplotlib.pyplot as plt
+import matplotlib.animation as animation
+from copy import deepcopy
 
 def cmp(a, b):
-    return (a > b) - (a < b) 
+    return (a > b) - (a < b)
 
 input = open("14_input").readlines()
 #input = open("14_test").readlines()
@@ -21,7 +25,7 @@ width = maxw - minw
 height = maxh - minh
 floor = maxh + 2
 
-grid = [['.'] * (width + 1) for ignored in range(floor + 1)]
+grid = [[0] * (width + 1) for ignored in range(floor + 1)]
 
 
 for line in input:
@@ -32,45 +36,59 @@ for line in input:
         x = prev_point[0] - minw
         y = prev_point[1]- minh
         while x != point[0] - minw or y != point[1] - minh:
-            grid[y][x] = "#"
+            grid[y][x] = 1
             x = x + cmp((point[0] - minw), x) 
             y = y + cmp((point[1] - minh), y)
-        grid[y][x] = "#"
+        grid[y][x] = 1
         prev_point = point
-grid[0 - minh][500 - minw] = "+"
-for x in grid: print("".join(x))
+grid[0 - minh][500 - minw] = 2
+for x in grid: print("".join(map(str, x)))
 
 # drop sand
 
+frames = []
 sy = 0
 sx = 0
 mod = 0
 sand = 0
 while True:
+    frames.append(deepcopy(grid))
     sx = (500 - minw) + mod
     sy = 0 - minh
     while True:
         if sx <= 0:
-            grid = [["."] + x for x in grid]
+            grid = [[0] + x for x in grid]
             mod += 1
             break
         elif sx >= len(grid[0]) - 1:
-            grid = [x + ["."] for x in grid]
+            grid = [x + [0] for x in grid]
             break
         elif sy >= len(grid) - 1:
-            grid[sy][sx] = "o"
+            grid[sy][sx] = 3
             break
-        elif grid[sy + 1][sx] == ".":
+        elif grid[sy + 1][sx] == 0:
             sy += 1
-        elif grid[sy + 1][sx - 1] == ".":
+        elif grid[sy + 1][sx - 1] == 0:
             sy += 1
             sx -= 1
-        elif grid[sy + 1][sx + 1] == ".":
+        elif grid[sy + 1][sx + 1] == 0:
             sy += 1
             sx += 1
         else:
-            grid[sy][sx] = "o"
+            grid[sy][sx] = 3
             sand += 1
             break
     if sx == (500 - minw) + mod and sy == 0 - minh: break
 print(sand)
+
+fig, ax = plt.subplots()
+
+ims = []
+for i in frames:
+    im = plt.imshow(i, animated = True)
+    ims.append([im])
+
+ani = animation.ArtistAnimation(fig, ims, interval=50, blit=True, repeat_delay=1000)
+
+ani.save("input_animation.mp4")
+plt.show()

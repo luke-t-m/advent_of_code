fce0b4ff3e2b42d67db27fd0e52d3ba716be6b96
look <luketmckay@outlook.com>
2022-12-15 13:51:19 +0000

    attempt to optimise day 15 with less success than hoped

--- 7_2022/f_day15_python/15.py
+++ 7_2022/f_day15_python/15.py
@@ -1,45 +1,54 @@
 #!/usr/bin/env python3
 import re
 
-def mergeIntervals(arr):
-    arr.sort(key=lambda x: x[0])
-    index = 0
-    for i in range(1, len(arr)):
-        if (arr[index][1] >= arr[i][0]):
-            arr[index][1] = max(arr[index][1], arr[i][1])
-        else:
-            index = index + 1
-            arr[index] = arr[i]
-    return arr[:index+1]
 
-def cant_ranges(y):
+def merge_ranges(ranges):
+    ranges.sort()
+    index = 0
+    for i in range(1, len(ranges)):
+        if ranges[index][1] >= ranges[i][0]:
+            ranges[index][1] = max(ranges[index][1], ranges[i][1])
+        else:
+            index += 1
+            ranges[index] = ranges[i]
+    return ranges[:index+1]
+
+def near_ranges(y, sensbeacs):
     ranges = []
-    for s in sensors:
+    for s in sensbeacs:
         sx, sy, bx, by = s
         manh = abs(sx - bx) + abs(sy - by)
         to_y = abs(sy - y)
         c = manh - to_y
         if c > 0: ranges.append([sx-abs(c), sx+abs(c)])
-    return mergeIntervals(ranges)
+    return ranges
 
-def part_one(y):
-    return sum(map(lambda i: i[1] - i[0], cant_ranges(y)))
+def part_one(y, sensbeacs):
+    return sum(map(lambda i: i[1] - i[0], merge_ranges(near_ranges(y, sensbeacs))))
 
-def part_two(max_xy):
-    for y in range(max_xy):
+def part_two(max_xy, sensbeacs):
+    for y in range(max_xy, -1, -1):
         if y % 10000 == 0: print(f"Progress: y = {y}", end='\r')
-        cants = cant_ranges(y)
+        cants = merge_ranges(near_ranges(y, sensbeacs))
         if len(cants) > 1:
             assert cants[1][0] - cants[0][1] == 2
-            return 4000000 * (cants[1][0] + 1) + y
+            return 4000000 * (cants[0][1] + 1) + y
 
-input = open("15_input").readlines()
+test = 0
 
-sensors = []
+if test:
+    input = open("15_test").readlines()
+    y, max_xy = 10, 20
+else:
+    input = open("15_input").readlines()
+    y, max_xy = 2000000, 4000000
+
+
+sensbeacs = []
 for line in input:
     r = re.search("Sensor at x=(-?\d*), y=(-?\d*): closest beacon is at x=(-?\d*), y=(-?\d*)", line)
-    sensors.append(list(map(int, [r.group(x) for x in range(1,5)])))
+    sensbeacs.append(list(map(int, [r.group(x) for x in range(1,5)])))
 
 
-print(f"Part one: {part_one(2000000)}")
-print(f"Part two: {part_two(4000000)}")
+print(f"Part one: {part_one(y, sensbeacs)}")
+print(f"Part two: {part_two(max_xy, sensbeacs)}")

91554e5edcfa1eaa4e4232e8b9d7e5777c95e3f4
look <luketmckay@outlook.com>
2022-12-15 19:26:17 +0000

    fixed day 4 python

--- 7_2022/4_day4_python/4.py
+++ 7_2022/4_day4_python/4.py
@@ -6,10 +6,9 @@ lines = file.readlines()
 file.close()
 
 lines = map(lambda x: re.split("-|,", x[:-1]), lines)
-lines = map(lambda x: list(map(int, x)), lines)
+lines = list(map(lambda x: list(map(int, x)), lines))
 
-p1 = [x for x in lines if (x[0] >= x[2] and x[1] <= x[3]) or (x[2] >= x[0] and x[3] <= x[1])] # whyt this breaking?????
-p2 = [x for x in lines if (x[1] >= x[2] and x[0] <= x[3]) or (x[0] >= x[3] and x[1] <= x[2])]
-print(p2)
-#print(f"Part one: {len(p1)}")
-print(f"Part two: {len(p2)}")
+
+
+print(f"Part one: {len([a for a, b, c, d in lines if (a >= c and b <= d) or (c >= a and d <= b)])}")
+print(f"Part two: {len([a for a, b, c, d in lines if (b >= c and a <= d) or (a >= d and b <= c)])}")

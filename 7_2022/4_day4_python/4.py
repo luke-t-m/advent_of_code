e778601383b41f309817b789cd013bc5a2d7c4d3
look <luketmckay@outlook.com>
2022-12-17 13:57:24 +0000

    day 16 python optimised, and day 4 fixed.

--- 7_2022/4_day4_python/4.py
+++ 7_2022/4_day4_python/4.py
@@ -9,6 +9,5 @@ lines = map(lambda x: re.split("-|,", x[:-1]), lines)
 lines = list(map(lambda x: list(map(int, x)), lines))
 
 
-
 print(f"Part one: {len([a for a, b, c, d in lines if (a >= c and b <= d) or (c >= a and d <= b)])}")
 print(f"Part two: {len([a for a, b, c, d in lines if (b >= c and a <= d) or (a >= d and b <= c)])}")

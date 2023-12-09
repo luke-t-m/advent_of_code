bf69292f06331b28c25a4737914cfcbe79f993f1
luke <luketmckay@outlook.com>
2023-12-09 08:22:39 +0000

    day 1 was missing, sitting in home. Added.

---
+++ aoc23/days/01/01.py
@@ -0,0 +1,19 @@
+file = open("aoc_in")
+raw = file.readlines()
+file.close()
+
+# sleep deprivation inspiration
+
+
+
+
+subs = ["one", "two", "three", "four", "five", "six", "seven", "eight", "nine"]
+sum = 0
+for line in raw:
+  for num, word in enumerate(subs):
+      line = line.replace(word, word[0] + str(num+1) + word[-1])
+  line = ''.join(c for c in line if c.isdigit())
+  sum += int(line[0] + line[-1])
+
+
+print(sum)

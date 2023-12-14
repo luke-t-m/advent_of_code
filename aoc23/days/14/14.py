5f6c7d86d3257dc201fc01292d69dd893fab70d3
luke <luketmckay@outlook.com>
2023-12-14 06:02:29 +0000

    tidied up code a bit

--- aoc23/days/14/14.py
+++ aoc23/days/14/14.py
@@ -1,76 +1,52 @@
 from copy import deepcopy
-import numpy as np
+
+def slide_up(grid):
+    changed = True
+    while changed:
+        changed = False
+        for y in range(len(grid) - 1, 0, -1):
+            for x in range(len(grid[0])):
+                if grid[y][x] == "O" and grid[y-1][x] == ".":
+                    grid[y][x] = "."
+                    grid[y-1][x] = "O"
+                    changed = True
+
+
+def rotated(grid):
+    return list([list(i) for i in zip(*grid[::-1])])
+
+
+def score(grid):
+    return sum(i * row.count("O") for i, row in enumerate(grid[::-1], 1))
+
 
 with open("input") as file:
     raw = file.read()
 
-if 0: raw = """
-O....#....
-O.OO#....#
-.....##...
-OO.#O....O
-.O.....O#.
-O.#..O.#.#
-..O..#O..O
-.......O..
-#....###..
-#OO..#....
-"""
-
 grid = [list(i) for i in raw.strip().split("\n")]
-og = deepcopy(grid)
-p1 = p2 = 0
-
-
-def slide_up(grid):
-    changes = 0
-    for y in range(len(grid) - 1, 0, -1):
-        for x in range(len(grid[0])):
-            if grid[y][x] == "O" and grid[y-1][x] == ".":
-                grid[y][x] = "."
-                grid[y-1][x] = "O"
-                changes += 1
-    return changes
-
-
-def print_grid():
-    for row in grid:
-        print("".join(row))
-
-
-while (changes := slide_up(grid)) != 0:
-    pass
-
-
-
-
-p1 = sum(i * row.count("O") for i, row in enumerate(grid[::-1], 1))
+slide_up(grid)
+p1 = score(grid)
 
 seen = []
-grid = og
+grid = [list(i) for i in raw.strip().split("\n")]
 skip = None
 cycles = 0
-while cycles != 1000000000:
-    print(cycles)
-    if skip != None and (cycles + 100 * skip < 1000000000):
-        cycles += 100 * skip
-        continue
-    if skip != None and (cycles + skip < 1000000000):
-        cycles += skip
-        continue
+want = 1000000000
+while cycles != want:
+    if skip is not None and (cycles + 100 * skip < 1000000000):
+        cycles += ((want - cycles) // skip) * skip
+
     for i in range(4):
-        while (changes := slide_up(grid)) != 0:
-            pass
-        grid = list([list(i) for i in zip(*grid[::-1])])
+        slide_up(grid)
+        grid = rotated(grid)
 
     if skip is None and grid in seen:
-        o = seen.index(grid)
-        skip = len(seen) - o
-    seen.append(deepcopy(grid))
-    p2 = sum(i * row.count("O") for i, row in enumerate(grid[::-1], 1))
-    print(cycles, p2)
+        skip = len(seen) - seen.index(grid)
+    else:
+        seen.append(deepcopy(grid))
+
     cycles += 1
 
-p2 = sum(i * row.count("O") for i, row in enumerate(grid[::-1], 1))
+p2 = score(grid)
 print(p1, p2)
 

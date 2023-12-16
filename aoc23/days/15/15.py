e701a5af0c078051d12c1679b7b2ca85220e8569
luke <luketmckay@outlook.com>
2023-12-16 16:32:27 +0000

    day 16. bad reflect function, brain not intuiting truth-esque tables

---
+++ aoc23/days/15/15.py
@@ -0,0 +1,36 @@
+with open("input") as file:
+    raw = file.read()
+
+
+def hash(string):
+    out = 0
+    for c in string:
+        out += ord(c)
+        out *= 17
+        out %= 256
+    return out
+
+
+parts = raw.strip().split(",")
+
+p1 = sum(hash(i) for i in parts)
+
+boxes = [dict() for _ in range(256)]
+
+for part in parts:
+    if part[-1] == "-":
+        label = part[:-1]
+        label_hash = hash(label)
+        boxes[label_hash] = {l: le for l, le in boxes[label_hash].items() if l != label}
+    elif part[-2] == "=":
+        label, lens = part.split("=")
+        boxes[hash(label)][label] = int(lens)
+
+
+p2 = sum(
+    (1 + i) * (1 + i2) * lens
+    for i, box in enumerate(boxes)
+    for i2, (_, lens) in enumerate(box.items())
+)
+
+print(p1, p2)

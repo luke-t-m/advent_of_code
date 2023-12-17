d2ed12eb54068aee2501e9d15d58e678549dfcc8
luke <luketmckay@outlook.com>
2023-12-17 10:40:09 +0000

    removed test string

--- aoc23/days/17/17.py
+++ aoc23/days/17/17.py
@@ -40,25 +40,7 @@ def solve(grid, start, goal, min_move, max_move):
 with open("input") as file:
     raw = file.read()
 
-if 0:
-    raw = """
-2413432311323
-3215453535623
-3255245654254
-3446585845452
-4546657867536
-1438598798454
-4457876987766
-3637877979653
-4654967986887
-4564679986453
-1224686865563
-2546548887735
-4322674655533
-"""
-
 lines = raw.strip().split("\n")
-
 grid = make_grid(lines)
 goal = max(grid.keys())
 

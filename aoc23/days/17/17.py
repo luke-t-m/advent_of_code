89ef216e5a0cd9ba4449fb5d54648bc67eedf09d
luke <luketmckay@outlook.com>
2023-12-17 10:39:27 +0000

    day 17, minimum cost path with a heap

---
+++ aoc23/days/17/17.py
@@ -0,0 +1,68 @@
+import heapq
+
+DIRECTIONS = [(0, 1), (1, 0), (0, -1), (-1, 0)]
+
+
+def make_grid(lines):
+    return {
+        (x, y): int(cell) for y, row in enumerate(lines) for x, cell in enumerate(row)
+    }
+
+
+def solve(grid, start, goal, min_move, max_move):
+    seen = set()
+    queue = [(0, start, None, 0)]
+    heapq.heapify(queue)
+
+    while len(queue) != 0:
+        cost, at, dir, consec = heapq.heappop(queue)
+
+        if at == goal and consec >= min_move:
+            return cost
+
+        state = (at, dir, consec)
+        if state in seen:
+            continue
+        seen.add(state)
+        if dir is not None and consec < max_move:
+            n_at = tuple(i + j for (i, j) in zip(at, dir))
+            if n_at in grid:
+                heapq.heappush(queue, (cost + grid[n_at], n_at, dir, consec + 1))
+        if consec >= min_move or dir is None:
+            for dir2 in DIRECTIONS:
+                if dir2 == dir or dir == (-dir2[0], -dir2[1]):
+                    continue
+                n_at = tuple(i + j for (i, j) in zip(at, dir2))
+                if n_at in grid:
+                    heapq.heappush(queue, (cost + grid[n_at], n_at, dir2, 1))
+
+
+with open("input") as file:
+    raw = file.read()
+
+if 0:
+    raw = """
+2413432311323
+3215453535623
+3255245654254
+3446585845452
+4546657867536
+1438598798454
+4457876987766
+3637877979653
+4654967986887
+4564679986453
+1224686865563
+2546548887735
+4322674655533
+"""
+
+lines = raw.strip().split("\n")
+
+grid = make_grid(lines)
+goal = max(grid.keys())
+
+p1 = solve(grid, (0, 0), goal, 0, 3)
+p2 = solve(grid, (0, 0), goal, 4, 10)
+
+print(p1, p2)

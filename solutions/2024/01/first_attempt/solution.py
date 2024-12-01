71773b8e74af2cf62755bb1526c7fe0b6dd63d05
Luke McKay <luketmckay@outlook.com>
2024-12-01 05:08:49 +0000

    stayed up all night and wrote a quick python solution, 684 global leaderboard. Downloader seems to work as intended first try, which (even though pointing this out is very cliche) i will point out is a bloddy miracle.

--- solutions/2024/01/first_attempt/solution.py
+++ solutions/2024/01/first_attempt/solution.py
@@ -6,6 +6,25 @@ input = sys.argv[1]
 
 lines = input.strip().split("\n")
 
+aa = []
+bb = []
+
+for l in lines:
+    a, b = l.split("   ")
+    aa.append(int(a))
+    bb.append(int(b))
+
+for a in aa:
+    p2 += bb.count(a) * a
+
+while len(aa) != 0:
+    smallest_a = min(aa)
+    aa.remove(smallest_a)
+    smallest_b = min(bb)
+    bb.remove(smallest_b)
+    p1 += abs(smallest_a - smallest_b)
+
+
 
 
 print(p1, p2)

f3d3ba8b53988a9130ecf90748f4d89d5d400a65
Luke McKay <luketmckay@outlook.com>
2024-12-12 09:58:45 +0000

    Very pleased with thinking for part two- was in jeapordy after realising 'checking' if the space above the permiter-adjacent wasn't part of the region would fail on internal sides- but checking if the combined check and perimiter WAS part of the region will catch them.

---
+++ solutions/2024/12/first_attempt/solution.py
@@ -0,0 +1,108 @@
+#!/usr/bin/python3
+import sys
+from subprocess import run as subprocess_run
+from collections import defaultdict, deque
+
+def output(val):
+    if val != 0:
+        subprocess_run(["xsel", "--clipboard"], input=f"{val}".encode())
+    print(val)
+
+def addT(a, b):
+    a1, a2 = a
+    b1, b2 = b
+    return (a1+b1, a2+b2)
+
+
+p1 = p2 = 0
+inp = sys.argv[1]
+
+inp2 = """
+OOOOO
+OXOXO
+OOOOO
+OXOXO
+OOOOO
+"""
+
+inp2 = """
+AAAA
+BBCD
+BBCC
+EEEC
+"""
+
+inp2 = """
+EEEEE
+EXXXX
+EEEEE
+EXXXX
+EEEEE
+"""
+
+dirs = [(0, 1), (0, -1), (1, 0), (-1, 0)]
+dir_checks = {(0, 1): (1, 0), (0, -1): (1, 0), (1, 0): (0, 1), (-1, 0): (0, 1)}
+
+rows = inp.strip().split("\n")
+letgrid = defaultdict(lambda: None)
+regions = defaultdict(lambda: [])
+
+for r, i in enumerate(rows):
+    for c, val in enumerate(i):
+        at = (r, c)
+        letgrid[at] = val
+
+id = 0
+seen = set()
+for r, _ in enumerate(rows):
+    for c, _ in enumerate(i):
+        at = (r, c)
+        if at in seen:
+            continue
+        dq = deque([at])
+        region = set([at])
+        while dq:
+            at = dq.pop()
+            if at in seen:
+                continue
+            for dir in dirs:
+                nat = addT(at, dir)
+                if letgrid[nat] == letgrid[at]:
+                    region.add(nat)
+                    dq.append(nat)
+                    seen.add(at)
+        if len(region) != 0:
+            regions[id] = region
+            id += 1
+
+for id in regions:
+    per = 0
+    for at in regions[id]:
+        for dir in dirs:
+            if letgrid[addT(at, dir)] != letgrid[at]:
+                per += 1
+    p1 += per * len(regions[id])
+
+for id in regions:
+    per = 0
+    for at in regions[id]:
+        for dir in dirs:
+            check = dir_checks[dir]
+            checked = addT(at, check)
+            if letgrid[addT(at, dir)] != letgrid[at] and (letgrid[checked] != letgrid[at]
+                                                        or letgrid[addT(checked, dir)] == letgrid[at]):
+                per += 1
+    p2 += per * len(regions[id])
+
+"""
+for r, _ in enumerate(rows):
+    for c, _ in enumerate(i):
+        print(f"{idgrid[(r, c)]}".ljust(3), end = "")
+    print()
+
+for id in regions:
+    print(id, letgrid[regions[id][0]], pers[id], len(regions[id]))
+"""
+
+output(p1)
+output(p2)
